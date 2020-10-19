3438b7e849fb72dad6efca6a5e1c594f0988294a
deepak-karkala <dkarkala01@gmail.com>
2020-10-20 05:19:02 +0530

    Basic version of webapp in flask

---
+++ webapp/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask
+import flask
+from flask_bootstrap import Bootstrap
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        #DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Landing page
+    @app.route('/', methods=['GET', 'POST'])
+    def hello():
+        # return 'Hello, World!'
+        
+        # Return landing page
+        if flask.request.method == 'GET':
+            return(flask.render_template('base.html'))
+
+    Bootstrap(app)
+
+    return app
