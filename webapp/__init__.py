38700b7c2a54199668239e9fafd26fa7e7180255
deepak-karkala <dkarkala01@gmail.com>
2020-10-21 09:39:17 +0530

    Model prediction verified with user input

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,12 +5,13 @@ import flask
 from flask_bootstrap import Bootstrap
 import sklearn
 import joblib
+import pandas as pd
 
 # Load pre-trained machine learning model.
 base_path = "/Users/nesara/Documents/aim/cs/projects/airbnb-data-science/webapp/";
 #with open(base_path + 'static/model/decision_tree.pkl', 'rb') as f:
 #    model = pickle.load(f)
-model = joblib.load(base_path + "static/model/decision_tree.pkl")
+model = joblib.load(base_path + "static/model/fullpipeline_linearregression.pkl")
 
 
 def create_app(test_config=None):
@@ -50,12 +51,7 @@ def create_app(test_config=None):
             # Dropdowns
             country = flask.request.form.get('combobox_country')
             city = flask.request.form.get('combobox_city')
-
             neighbourhood = flask.request.form.get('combobox_neighbourhood')
-            if neighbourhood == "":
-                neighbourhood = "Downtown Brooklyn"
-
-
             propertytype = flask.request.form.get('combobox_propertytype')
             roomtype = flask.request.form.get('combobox_roomtype')
             bedtype = flask.request.form.get('combobox_bedtype')
@@ -65,6 +61,50 @@ def create_app(test_config=None):
 
             # Text input
             accommodates = flask.request.form['accommodates']
+            num_bedrooms = flask.request.form['num_bedrooms']
+            num_beds = flask.request.form['num_beds']
+            min_nights = flask.request.form['min_nights']
+            availability_30 = flask.request.form['availability_30']
+            availability_60 = 12
+            availability_90 = 12
+            availability_365 = flask.request.form['availability_365']
+            num_reviews = flask.request.form['num_reviews']
+            reviews_per_month = 0.22
+            review_scores_rating = flask.request.form['review_scores_rating']
+            review_scores_accuracy = flask.request.form['review_scores_accuracy']
+            review_scores_cleanliness = flask.request.form['review_scores_cleanliness']
+            review_scores_checkin = flask.request.form['review_scores_checkin']
+            review_scores_communication = flask.request.form['review_scores_communication']
+            review_scores_location = flask.request.form['review_scores_location']
+            review_scores_value = flask.request.form['review_scores_value']
+            host_since = flask.request.form['host_since']
+            host_response_rate = flask.request.form['host_response_rate']
+            host_num_listings = flask.request.form['host_num_listings']
+
+
+
+            # Create input to Model from form data
+            df_input = pd.DataFrame([[country, city, neighbourhood, propertytype, roomtype, bedtype,
+                                    cancellationpolicy, hostresponsetime, accommodates, num_bedrooms, num_beds,
+                                    min_nights, availability_30, availability_60, availability_90, availability_365,
+                                    num_reviews, reviews_per_month, review_scores_rating, review_scores_accuracy, 
+                                    review_scores_cleanliness, review_scores_checkin, review_scores_communication,
+                                    review_scores_location, review_scores_value, host_response_rate,
+                                    ]],
+                                           columns=['Country', 'City', 'Neighbourhood Cleansed', 'Property Type',
+                                                   'Room Type', 'Bed Type', 'Cancellation Policy', 'Host Response Time',
+                                                   'Accommodates', 'Bedrooms', 'Beds', 'Minimum Nights', 'Availability 30',
+                                                   'Availability 60', 'Availability 90', 'Availability 365',
+                                                   'Number of Reviews', 'Reviews per Month', 'Review Scores Rating',
+                                                   'Review Scores Accuracy', 'Review Scores Cleanliness',
+                                                   'Review Scores Checkin', 'Review Scores Communication',
+                                                   'Review Scores Location', 'Review Scores Value', 'Host Response Rate'],
+                                           dtype=float)
+
+            # Inference: Get prediction from Model
+            prediction_price = model.predict(df_input)[0]
+
+
 
 
             return(flask.render_template('base.html',   original_input={    'country': country,
@@ -76,8 +116,24 @@ def create_app(test_config=None):
                                                                             'cancellationpolicy': cancellationpolicy,
                                                                             'hostresponsetime': hostresponsetime,
                                                                             'accommodates': accommodates,
+                                                                            'num_bedrooms': num_bedrooms,
+                                                                            'num_beds': num_beds,
+                                                                            'min_nights': min_nights,
+                                                                            'availability_30': availability_30,
+                                                                            'availability_365': availability_365,
+                                                                            'num_reviews': num_reviews,
+                                                                            'review_scores_rating': review_scores_rating,
+                                                                            'review_scores_accuracy': review_scores_accuracy,
+                                                                            'review_scores_cleanliness': review_scores_cleanliness,
+                                                                            'review_scores_checkin': review_scores_checkin,
+                                                                            'review_scores_communication': review_scores_communication,
+                                                                            'review_scores_location': review_scores_location,
+                                                                            'review_scores_value': review_scores_value,
+                                                                            'host_since': host_since,
+                                                                            'host_response_rate': host_response_rate,
+                                                                            'host_num_listings': host_num_listings,
                                                                         },
-                                                        result=5))
+                                                        result=prediction_price))
 
             """
             # Read variables from form
