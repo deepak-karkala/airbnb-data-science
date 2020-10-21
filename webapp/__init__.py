569fcbedce7e6b0004601d8dad052f282286a00f
deepak-karkala <dkarkala01@gmail.com>
2020-10-21 07:59:18 +0530

    Updated view and controller

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,17 @@
 import os
-
+import pickle
 from flask import Flask
 import flask
 from flask_bootstrap import Bootstrap
+import sklearn
+import joblib
+
+# Load pre-trained machine learning model.
+base_path = "/Users/nesara/Documents/aim/cs/projects/airbnb-data-science/webapp/";
+#with open(base_path + 'static/model/decision_tree.pkl', 'rb') as f:
+#    model = pickle.load(f)
+model = joblib.load(base_path + "static/model/decision_tree.pkl")
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -33,6 +42,65 @@ def create_app(test_config=None):
         # Return landing page
         if flask.request.method == 'GET':
             return(flask.render_template('base.html'))
+
+
+        # Return prediction output
+        if flask.request.method == 'POST':
+
+            # Dropdowns
+            country = flask.request.form.get('combobox_country')
+            city = flask.request.form.get('combobox_city')
+
+            neighbourhood = flask.request.form.get('combobox_neighbourhood')
+            if neighbourhood == "":
+                neighbourhood = "Downtown Brooklyn"
+
+
+            propertytype = flask.request.form.get('combobox_propertytype')
+            roomtype = flask.request.form.get('combobox_roomtype')
+            bedtype = flask.request.form.get('combobox_bedtype')
+            cancellationpolicy = flask.request.form.get('combobox_cancellationpolicy')
+            hostresponsetime = flask.request.form.get('combobox_hostresponsetime')
+
+
+            # Text input
+            accommodates = flask.request.form['accommodates']
+
+
+            return(flask.render_template('base.html',   original_input={    'country': country,
+                                                                            'city': city,
+                                                                            'neighbourhood': neighbourhood,
+                                                                            'propertytype': propertytype,
+                                                                            'roomtype': roomtype,
+                                                                            'bedtype': bedtype,
+                                                                            'cancellationpolicy': cancellationpolicy,
+                                                                            'hostresponsetime': hostresponsetime,
+                                                                            'accommodates': accommodates,
+                                                                        },
+                                                        result=5))
+
+            """
+            # Read variables from form
+            temperature = flask.request.form['temperature']
+            humidity = flask.request.form['humidity']
+            windspeed = flask.request.form['windspeed']
+
+            # Create input to Model from form data
+            input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
+                                           columns=['temperature', 'humidity', 'windspeed'],
+                                           dtype=float)
+
+            # Inference: Get prediction from Model
+            prediction = model.predict(input_variables)[0]
+
+            # Return Model output
+            return flask.render_template('main.html',
+                                         original_input={'Temperature':temperature,
+                                                         'Humidity':humidity,
+                                                         'Windspeed':windspeed},
+                                         result=prediction,
+                                         )
+            """
 
     Bootstrap(app)
 
