33191aee4ff4317fd3b528c052ee0b81091d1a8a
deepak-karkala <dkarkala01@gmail.com>
2020-12-02 19:26:14 +0530

    First Docker image on AWS

--- webapp_predict_price/__init__.py
+++ webapp_predict_price/__init__.py
@@ -220,4 +220,4 @@ if __name__ == "__main__":
     print(("* Loading Scikit-learn model and Flask starting server..."
         "please wait until server has fully started"))
     app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
